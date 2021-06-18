aeef39855f75cb8c7db298a1076a72a841c5f94d
Jaspe Hsu <jasperhsu03@jcg-tech.com>
2021-06-18 08:22:54 +0800

    [update]

--- client/client.go
+++ client/client.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
+
+	pb "../pb"
 
 	"google.golang.org/grpc"
 )
@@ -12,5 +15,14 @@ func main() {
 		log.Fatalf("連線失敗：%v", err)
 	}
 	defer conn.Close()
+
+	c := pb.NewEchoServerClient(conn)
+
+	r, err := c.Echo(context.Background(), &pb.EchoRequest{Message: "HI HI HI HI"})
+	if err != nil {
+		log.Fatalf("無法執行 Plus 函式：%v", err)
+	}
+
+	log.Printf("回傳結果：%s , 時間:%d", r.Message, r.Unixtime)
 
 }
--- server/server.go
+++ server/server.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	pb "../pb"
+	"context"
+	"log"
+	"time"
+)
 
+type EchoServer struct{}
+
+func (e *EchoServer) Echo(ctx context.Context, req *pb.EchoRequest) (resp *pb.EchoReply, err error) {
+
+	log.Printf("receive client request, client send:%s\n", req.Msg)
+	return &pb.EchoReply{
+		Msg:      req.Msg,
+		Unixtime: time.Now().Unix(),
+	}, nil
+
+}
+
+func main() {
+	apiListener, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// 註冊 grpc
+	es := &EchoServer{}
+
+	grpc := grpc.NewServer()
+
+	pb.RegisterEchoServer(grpc, es)
+
+	reflection.Register(grpc)
+	if err := grpc.Serve(apiListener); err != nil {
+		log.Fatal(" grpc.Serve Error: ", err)
+		return
+	}
 }
